6a6b0ce1f8344a7e605032944a764fc8825a0826
Bektur Akylbek uulu <officialbektur@gmail.com>
2024-06-19 17:30:22 +0600

    update

--- app/Http/Controllers/Api/Post/ShowController.php
+++ app/Http/Controllers/Api/Post/ShowController.php
@@ -3,11 +3,8 @@
 namespace App\Http\Controllers\Api\Post;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Post\PostIndexRequest;
-use App\Http\Resources\Post\PostIndexResource;
 use App\Http\Resources\Post\PostFullResource;
 use App\Models\Post;
-use Illuminate\Http\JsonResponse;
 
 class ShowController extends Controller
 {
